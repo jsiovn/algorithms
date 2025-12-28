fd79d941dcbccf16540ed14b4ea9501637b56cea
Paolo Phan <phudev95@gmail.com>
2025-12-28 11:41:45 +0700

    add dft

---
+++ src/algorithms/06-trees/03-depth-first-tree-traversals.ts
@@ -0,0 +1,23 @@
+export const preorderTraverse = (node: any, array: number[]): number[] => {
+  if (!node) return array;
+  array.push(node.value);
+  array = preorderTraverse(node.left, array);
+  array = preorderTraverse(node.right, array);
+  return array;
+};
+
+export const inorderTraverse = (node: any, array: number[]): number[] => {
+  if (!node) return array;
+  array = inorderTraverse(node.left, array);
+  array.push(node.value);
+  array = inorderTraverse(node.right, array);
+  return array;
+};
+
+export const postorderTraverse = (node: any, array: number[]): number[] => {
+  if (!node) return array;
+  array = postorderTraverse(node.left, array);
+  array = postorderTraverse(node.right, array);
+  array.push(node.value);
+  return array;
+};
